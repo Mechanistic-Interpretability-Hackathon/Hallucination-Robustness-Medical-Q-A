e189f04a697e0e0b94b48e182986dc2a26f189d1
Eitan-Sprejer <eitusprejer@gmail.com>
2025-01-10 12:39:03 +0200

    sh script to run baseline hallucination analysis

--- run_baseline_hallucination.sh
+++ run_baseline_hallucination.sh
@@ -5,5 +5,5 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
 # Execute the command with the current parameters
 python src/hallucination_llm_evaluation/baseline_hallucination.py \
-  --n_examples 900 \
+  --n_examples 10000 \
   --batch_size 100
